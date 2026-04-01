b81e437de5dfe89ce94e0df04a0919f3023837da
priymavani <priy.mavani.cg@gmail.com>
2026-04-01 14:16:43 +0530

    Add application properties and initial test class for Student CRUD application

---
+++ src/test/java/com/college/studentcrud/StudentcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.college.studentcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
