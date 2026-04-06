2ca0380cd801aa0b3e10a33887563e4543b2420c
priymavani <priy.mavani.cg@gmail.com>
2026-04-06 14:15:54 +0530

    Implement student retrieval and search functionalities in StudentController

--- src/main/java/com/college/studentcrud/controller/StudentController.java
+++ src/main/java/com/college/studentcrud/controller/StudentController.java
@@ -5,7 +5,9 @@ import com.college.studentcrud.service.StudentService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
@@ -35,5 +37,42 @@ public class StudentController {
         // ResponseEntity is a class that represents the HTTP response including the status code, headers and body
         // ok method will return the response with status code 200 and the body will be the list of students
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id) {
+        Optional<Student> student = studentService.getStudentById(id);
+
+        if(student.isPresent()) {
+            return ResponseEntity.ok(student.get());
+        } 
+
+        return ResponseEntity.notFound().build();
+        
+        // build method will return the response with status code 404 and no body
     
+
+    }   
+
+    @GetMapping("/search")
+    public ResponseEntity<List<Student>> getStudentsByCourse(@RequestParam String course){
+
+        List<Student> students = studentService.getStudentByCourse(course);
+        return ResponseEntity.ok(students);
+
+    }
+
+    @GetMapping("/searchByName")
+    public ResponseEntity<List<Student>> searchByName(@RequestParam String name){
+
+        List<Student> students = studentService.searchByName(name);
+        return ResponseEntity.ok(students);
+
+    }
+    
+
+    @GetMapping("/count")
+    public ResponseEntity<String> getCount(){
+        Long count = studentService.getTotalCount();
+        return ResponseEntity.ok("Total Student in Database: " + count);
+    }
 }
